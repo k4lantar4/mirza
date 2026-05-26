efda829a19bb213f412421bedf0981e3054d68da
Mahdi <mahdigholipour@outlook.com>
2026-05-26 15:39:06 +0330

    refactor(keyboard): clean code and add text localization
    fix code spacing and indentation, add translation mapping for keyboard menu entries, and clean up loop syntax

--- api/keyboard.php
+++ api/keyboard.php
@@ -10,7 +10,7 @@ ini_set('error_log', 'error_log');
 
 
 $textbotlang = languagechange();
-$keyboardmain = json_decode(select("setting","keyboardmain",null,null,"select")['keyboardmain'],true);
+$keyboardmain = json_decode(select("setting", "keyboardmain", null, null, "select")['keyboardmain'], true);
 
 $list_keyboard = array(
     'text_sell',
@@ -23,18 +23,30 @@ $list_keyboard = array(
     'text_Tariff_list',
     'text_support',
     'text_help',
-    );
-foreach ($keyboardmain['keyboard'] as $keyboard){
-    foreach ($keyboard as $arrkey){
-            if(in_array($arrkey['text'],$list_keyboard)){
-                $index_number = array_search($arrkey['text'],$list_keyboard);
-                unset($list_keyboard[$index_number]);
+);
+$textbotlang['textbot'] = [
+    'text_sell' => $textbotlang['textbot']['sell'],
+    'text_extend' => $textbotlang['textbot']['extend'],
+    'text_usertest' => $textbotlang['textbot']['userTest'],
+    'text_wheel_luck' => $textbotlang['textbot']['wheelLuck'],
+    'text_Purchased_services' => $textbotlang['textbot']['purchasedServices'],
+    'accountwallet' => $textbotlang['textbot']['accountWallet'],
+    'text_affiliates' => $textbotlang['textbot']['affiliates'],
+    'text_Tariff_list' => $textbotlang['textbot']['tariffList'],
+    'text_support' => $textbotlang['textbot']['support'],
+    'text_help' => $textbotlang['textbot']['help'],
+];
+foreach ($keyboardmain['keyboard'] as $keyboard) {
+    foreach ($keyboard as $arrkey) {
+        if (in_array($arrkey['text'], $list_keyboard)) {
+            $index_number = array_search($arrkey['text'], $list_keyboard);
+            unset($list_keyboard[$index_number]);
         }
     }
-}    
+}
 $list_keyboard = array_values($list_keyboard);
 $keyboard = [];
-foreach($list_keyboard as $key){
+foreach ($list_keyboard as $key) {
     $keyboard[] = [['text' => $key]];
 }
 
