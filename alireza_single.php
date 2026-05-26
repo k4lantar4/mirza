9f206e397de464c4dba7d272c5113b209ce50577
Mahdi <mahdigholipour@outlook.com>
2026-05-26 11:22:43 +0330

    Refactor get_clinetsalireza and related functions for improved readability and consistency; streamline cURL options and response handling.

--- alireza_single.php
+++ alireza_single.php
@@ -4,78 +4,82 @@ require_once 'x-ui_single.php';
 require_once 'request.php';
 ini_set('error_log', 'error_log');
 
-function get_clinetsalireza($username,$namepanel){
-    $marzban_list_get = select("marzban_panel", "*", "name_panel", $namepanel,"select");
+function get_clinetsalireza($username, $namepanel)
+{
+    $marzban_list_get = select("marzban_panel", "*", "name_panel", $namepanel, "select");
     login($marzban_list_get['code_panel']);
     $curl = curl_init();
 
-curl_setopt_array($curl, array(
-  CURLOPT_URL => $marzban_list_get['url_panel'].'/xui/API/inbounds',
-  CURLOPT_RETURNTRANSFER => true,
-  CURLOPT_ENCODING => '',
-  CURLOPT_MAXREDIRS => 10,
-  CURLOPT_SSL_VERIFYHOST =>  false,
-  CURLOPT_SSL_VERIFYPEER => false,
-  CURLOPT_TIMEOUT_MS => 4000,
-  CURLOPT_FOLLOWLOCATION => true,
-  CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
-  CURLOPT_CUSTOMREQUEST => 'GET',
-  CURLOPT_HTTPHEADER => array(
-    'Accept: application/json'
-  ),
-  CURLOPT_COOKIEFILE => 'cookie.txt',
-));
-$output = [];
-$response = json_decode(curl_exec($curl),true)['obj'];
-if(!isset($response))return;
-foreach ($response as $client){
-    $clientdata= json_decode($client['settings'],true)['clients'];
-    foreach($clientdata as $clinets){
-    if($clinets['email'] == $username){
-        $output[] = $clinets;
-        break;
+    curl_setopt_array($curl, array(
+        CURLOPT_URL => $marzban_list_get['url_panel'] . '/xui/API/inbounds',
+        CURLOPT_RETURNTRANSFER => true,
+        CURLOPT_ENCODING => '',
+        CURLOPT_MAXREDIRS => 10,
+        CURLOPT_SSL_VERIFYHOST => false,
+        CURLOPT_SSL_VERIFYPEER => false,
+        CURLOPT_TIMEOUT_MS => 4000,
+        CURLOPT_FOLLOWLOCATION => true,
+        CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
+        CURLOPT_CUSTOMREQUEST => 'GET',
+        CURLOPT_HTTPHEADER => array(
+            'Accept: application/json'
+        ),
+        CURLOPT_COOKIEFILE => 'cookie.txt',
+    ));
+    $output = [];
+    $response = json_decode(curl_exec($curl), true)['obj'];
+    if (!isset($response))
+        return;
+    foreach ($response as $client) {
+        $clientdata = json_decode($client['settings'], true)['clients'];
+        foreach ($clientdata as $clinets) {
+            if ($clinets['email'] == $username) {
+                $output[] = $clinets;
+                break;
+            }
+        }
+        $clientStats = $client['clientStats'];
+        foreach ($clientStats as $clinetsup) {
+            if ($clinetsup['email'] == $username) {
+                $output[] = $clinetsup;
+                break;
+            }
+        }
+
     }
-    }
-    $clientStats= $client['clientStats'];
-    foreach($clientStats as $clinetsup){
-    if($clinetsup['email'] == $username){
-        $output[] = $clinetsup;
-        break;
-    }
-    }
-    
+    unlink('cookie.txt');
+    return $output;
 }
-unlink('cookie.txt');
-return $output;
-}
-function addClientalireza_singel($namepanel, $usernameac, $Expire,$Total, $Uuid,$Flow,$subid,$inboundid){
-    $marzban_list_get = select("marzban_panel", "*", "name_panel", $namepanel,"select");
+function addClientalireza_singel($namepanel, $usernameac, $Expire, $Total, $Uuid, $Flow, $subid, $inboundid)
+{
+    $marzban_list_get = select("marzban_panel", "*", "name_panel", $namepanel, "select");
     login($marzban_list_get['code_panel']);
     $config = array(
         "id" => intval($inboundid),
         'settings' => json_encode(array(
             'clients' => array(
                 array(
-                "id" => $Uuid,
-                "flow" => $Flow,
-                "email" => $usernameac,
-                "totalGB" => $Total,
-                "expiryTime" => $Expire,
-                "enable" => true,
-                "tgId" => "",
-                "subId" => $subid,
-                "reset" => 0
-            )),
-             'decryption' => 'none',
+                    "id" => $Uuid,
+                    "flow" => $Flow,
+                    "email" => $usernameac,
+                    "totalGB" => $Total,
+                    "expiryTime" => $Expire,
+                    "enable" => true,
+                    "tgId" => "",
+                    "subId" => $subid,
+                    "reset" => 0
+                )
+            ),
+            'decryption' => 'none',
             'fallbacks' => array(),
         ))
-        );
+    );
 
-    $configpanel = json_encode($config,true);
-    $url = $marzban_list_get['url_panel'].'/xui/API/inbounds/addClient';
+    $configpanel = json_encode($config, true);
+    $url = $marzban_list_get['url_panel'] . '/xui/API/inbounds/addClient';
     $headers = array(
-            'Accept: application/json',
-            'Content-Type: application/json',
+        'Accept: application/json',
+        'Content-Type: application/json',
     );
     $req = new CurlRequest($url);
     $req->setHeaders($headers);
@@ -84,15 +88,16 @@ function addClientalireza_singel($namepanel, $usernameac, $Expire,$Total, $Uuid,
     unlink('cookie.txt');
     return $response;
 }
-function updateClientalireza($namepanel, $username,array $config){
-    $UsernameData = get_clinetsalireza($username,$namepanel)[0];
-    $marzban_list_get = select("marzban_panel", "*", "name_panel", $namepanel,"select");
+function updateClientalireza($namepanel, $username, array $config)
+{
+    $UsernameData = get_clinetsalireza($username, $namepanel)[0];
+    $marzban_list_get = select("marzban_panel", "*", "name_panel", $namepanel, "select");
     login($marzban_list_get['code_panel']);
-    $configpanel = json_encode($config,true);
-    $url = $marzban_list_get['url_panel'].'/xui/API/inbounds/updateClient/'.$UsernameData['id'];
+    $configpanel = json_encode($config, true);
+    $url = $marzban_list_get['url_panel'] . '/xui/API/inbounds/updateClient/' . $UsernameData['id'];
     $headers = array(
-            'Accept: application/json',
-            'Content-Type: application/json',
+        'Accept: application/json',
+        'Content-Type: application/json',
     );
     $req = new CurlRequest($url);
     $req->setHeaders($headers);
@@ -101,14 +106,15 @@ function updateClientalireza($namepanel, $username,array $config){
     unlink('cookie.txt');
     return $response;
 }
-function ResetUserDataUsagealirezasin($usernamepanel, $namepanel){
-    $data_user = get_clinetsalireza($usernamepanel,$namepanel)[0];
-    $marzban_list_get = select("marzban_panel", "*", "name_panel", $namepanel,"select");
+function ResetUserDataUsagealirezasin($usernamepanel, $namepanel)
+{
+    $data_user = get_clinetsalireza($usernamepanel, $namepanel)[0];
+    $marzban_list_get = select("marzban_panel", "*", "name_panel", $namepanel, "select");
     login($marzban_list_get['code_panel']);
-    $url = $marzban_list_get['url_panel']."/xui/API/inbounds/{$marzban_list_get['inboundid']}/resetClientTraffic/".$data_user['email'];
+    $url = $marzban_list_get['url_panel'] . "/xui/API/inbounds/{$marzban_list_get['inboundid']}/resetClientTraffic/" . $data_user['email'];
     $headers = array(
-            'Accept: application/json',
-            'Content-Type: application/json',
+        'Accept: application/json',
+        'Content-Type: application/json',
     );
     $req = new CurlRequest($url);
     $req->setHeaders($headers);
@@ -117,14 +123,15 @@ function ResetUserDataUsagealirezasin($usernamepanel, $namepanel){
     unlink('cookie.txt');
     return $response;
 }
-function removeClientalireza_single($location,$username){
-    $marzban_list_get = select("marzban_panel", "*", "name_panel", $location,"select");
-    $data_user = get_clinetsalireza($username,$location)[0];
+function removeClientalireza_single($location, $username)
+{
+    $marzban_list_get = select("marzban_panel", "*", "name_panel", $location, "select");
+    $data_user = get_clinetsalireza($username, $location)[0];
     login($marzban_list_get['code_panel']);
-    $url = $marzban_list_get['url_panel']."/xui/API/inbounds/{$marzban_list_get['inboundid']}/delClient/".$data_user['id'];
+    $url = $marzban_list_get['url_panel'] . "/xui/API/inbounds/{$marzban_list_get['inboundid']}/delClient/" . $data_user['id'];
     $headers = array(
-            'Accept: application/json',
-            'Content-Type: application/json',
+        'Accept: application/json',
+        'Content-Type: application/json',
     );
     $req = new CurlRequest($url);
     $req->setHeaders($headers);
@@ -132,32 +139,35 @@ function removeClientalireza_single($location,$username){
     $response = $req->post(array());
     unlink('cookie.txt');
     return $response;
-    
+
 }
-function get_onlineclialireza($name_panel,$username){
-    $marzban_list_get = select("marzban_panel", "*", "name_panel", $name_panel,"select");
+function get_onlineclialireza($name_panel, $username)
+{
+    $marzban_list_get = select("marzban_panel", "*", "name_panel", $name_panel, "select");
     login($marzban_list_get['code_panel']);
     $curl = curl_init();
-curl_setopt_array($curl, array(
-  CURLOPT_URL => $marzban_list_get['url_panel'].'/xui/API/inbounds/onlines',
-  CURLOPT_RETURNTRANSFER => true,
-  CURLOPT_ENCODING => '',
-  CURLOPT_MAXREDIRS => 10,
-  CURLOPT_TIMEOUT => 0,
-  CURLOPT_SSL_VERIFYHOST =>  false,
-  CURLOPT_SSL_VERIFYPEER => false,
-  CURLOPT_FOLLOWLOCATION => true,
-  CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
-  CURLOPT_CUSTOMREQUEST => 'POST',
-  CURLOPT_HTTPHEADER => array(
-    'Accept: application/json'
-  ),
-  CURLOPT_COOKIEFILE => 'cookie.txt',
-));
-$response = json_decode(curl_exec($curl),true)['obj'];
-unlink('cookie.txt');
-if($response == null)return "offline";
-if(in_array($username,$response))return "online";
-return "offline";
+    curl_setopt_array($curl, array(
+        CURLOPT_URL => $marzban_list_get['url_panel'] . '/xui/API/inbounds/onlines',
+        CURLOPT_RETURNTRANSFER => true,
+        CURLOPT_ENCODING => '',
+        CURLOPT_MAXREDIRS => 10,
+        CURLOPT_TIMEOUT => 0,
+        CURLOPT_SSL_VERIFYHOST => false,
+        CURLOPT_SSL_VERIFYPEER => false,
+        CURLOPT_FOLLOWLOCATION => true,
+        CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
+        CURLOPT_CUSTOMREQUEST => 'POST',
+        CURLOPT_HTTPHEADER => array(
+            'Accept: application/json'
+        ),
+        CURLOPT_COOKIEFILE => 'cookie.txt',
+    ));
+    $response = json_decode(curl_exec($curl), true)['obj'];
+    unlink('cookie.txt');
+    if ($response == null)
+        return "offline";
+    if (in_array($username, $response))
+        return "online";
+    return "offline";
 
 }
